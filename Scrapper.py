477b0f003bb7f741a8b36b10b1424ea7957f00ed
Nethra1 <vijayadasnethra97@gmail.com>
2022-04-11 16:14:49 -0400

    merge conflict resolve

--- Scrapper.py
+++ Scrapper.py
@@ -1,8 +1,13 @@
-from asyncio.windows_events import NULL
+# from asyncio.windows_events import NULL
 from concurrent.futures import process
 from bs4 import BeautifulSoup
 import requests
 import csv
+
+from src.JobDetails import JobDetails
+
+jobsList = []
+
 
 def processHomePage(job_page_links):
     with open('pageLinks.csv', 'a', newline='', encoding='utf-8') as csvF:
@@ -19,32 +24,31 @@ def processHomePage(job_page_links):
             jobTitle = soup.find('div', class_='jobsearch-JobInfoHeader-title-container').h1.text
 
             company = ''
+
             if (soup.find('div', class_='jobsearch-CompanyInfoContainer') is not None and
                     soup.find('div', class_='jobsearch-CompanyInfoContainer').a is not None and 
                       soup.find('div', class_='jobsearch-CompanyInfoContainer').a.string is not None ):    
                 company = soup.find('div', class_='jobsearch-CompanyInfoContainer').a.string
             else:
-                company = 'null'
-
+                company = None
 
             # print(link + '------>')
             # print(mainDivJobPageDescription)
             # print('\n')
 
-            
-            # csv_writer.writerow(['links'])
-            # for content in mainDivJobPage.contents:
-            # base_link = "https://ca.indeed.com"
-            
 
-            if  link:
-                csv_writer.writerow([jobTitle, company, link, mainDivJobPageDescription ])
-                # job_page_links.append(link)
 
+            if link:
+                csv_writer.writerow([jobTitle, company, link, mainDivJobPageDescription])
+
+            job = JobDetails(jobTitle, company, link, mainDivJobPageDescription)
+            jobsList.append(job)
+            # print(job.jobTitle)
+    print(len(jobsList))
+    return jobsList
 
 
 def processSoup(source):
-
     soup = BeautifulSoup(source, 'lxml')
 
     # a list to store the job links 
@@ -60,16 +64,16 @@ def processSoup(source):
         base_link = "https://ca.indeed.com"
         link = base_link + content['href']
 
-        if  link:
+        if link:
             # csv_writer.writerow([link, mainDivJobPage])
             job_page_links.append(link)
-        
+
     # this opens the out put file
-    with open('outputs1.txt', 'a') as f: 
-        
+    with open('outputs1.txt', 'a') as f:
+
         # this get the all  the anchors in the main div that have the class tapItem. 
         # others anchors link to company pages, reviews etc. 
-        
+
         # for content in contents:
         #     link = content['href']
         #     f.write(link)
@@ -77,11 +81,10 @@ def processSoup(source):
         #     f.write('\n')
         #     job_page_links.append(link)
         pass
-    
+
     processHomePage(job_page_links)
-
-
-
+    print(len(jobsList))
+    return jobsList
 
     # with open('test.csv', 'r') as readCsv:
     #     csvReader = csv.reader(readCsv)
@@ -90,24 +93,32 @@ def processSoup(source):
     #         if not row:
     #             continue
     #         print(row)
-        
-
-pagenationsLinks = ['https://ca.indeed.com/jobs?q=&l=Ontario',
-                     'https://ca.indeed.com/jobs?l=Ontario&start=20',   
-                     'https://ca.indeed.com/jobs?l=Ontario&start=30',   
-                     'https://ca.indeed.com/jobs?l=Ontario&start=40',   
-                     'https://ca.indeed.com/jobs?l=Ontario&start=50'
-                        
-                    ]
-with open('pageLinks.csv', 'w', newline='', encoding='utf-8') as csvF:
-    csv_writer = csv.writer(csvF)
-    csv_writer.writerow(['Job Title', 'Company', 'Job Page link', 'Job Description' ])
-# with open('outputs1.txt', 'w') as csvF: 
-#     csv_writer = csv.writer(csvF)
-#     csv_writer.writerow(['links'])
-    for sourceUrl in pagenationsLinks:
-        source = requests.get(sourceUrl).text
-        processSoup(source)
 
 
-    
+def startCrawling():
+    pagenationsLinks = ['https://ca.indeed.com/jobs?q=&l=Ontario',
+                        'https://ca.indeed.com/jobs?l=Ontario&start=20',
+                        'https://ca.indeed.com/jobs?l=Ontario&start=30',
+                        'https://ca.indeed.com/jobs?l=Ontario&start=40',
+                        'https://ca.indeed.com/jobs?l=Ontario&start=50'
+
+                        ]
+    with open('pageLinks.csv', 'w', newline='', encoding='utf-8') as csvF:
+        csv_writer = csv.writer(csvF)
+        csv_writer.writerow(['Job Title', 'Company', 'Job Page link', 'Job Description'])
+        # with open('outputs1.txt', 'w') as csvF:
+        #     csv_writer = csv.writer(csvF)
+        #     csv_writer.writerow(['links'])
+        for sourceUrl in pagenationsLinks:
+            source = requests.get(sourceUrl).text
+            processSoup(source)
+    print(jobsList[0].jobTitle)
+    return jobsList
+
+
+def main():
+    print(startCrawling())
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/JobDetails.py
@@ -0,0 +1,14 @@
+class JobDetails:
+    # jobTitle = ""
+    # company = ""
+    # link = ""
+    # jobDescription = ""
+
+
+    def __init__(self, jobTitle, company,link,jobDescription):
+        self.jobTitle = jobTitle
+        self.company = company
+        self.link = link
+        self.jobDescription = jobDescription
+
+
