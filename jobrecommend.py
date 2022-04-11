7ab4e253a64aade92ab71964524cdd214f9edf0c
Nethra1 <vijayadasnethra97@gmail.com>
2022-04-11 15:41:28 -0400

    UI added

---
+++ jobrecommend.py
@@ -0,0 +1,20 @@
+from bottle import run, route, view, static_file
+
+from Scrapper import startCrawling
+
+
+@route('/')
+@view('index')
+def index():
+    jobList = startCrawling()
+    print(jobList[0].link)
+    return dict(
+        jobs = jobList
+    )
+    # pass
+
+@route('/static/<filepath:path>')
+def load_static(filepath):
+    return static_file(filepath, root='./static')
+# Start the website
+run(host='0.0.0.0', port=8080, reloader=True, debug=True)
